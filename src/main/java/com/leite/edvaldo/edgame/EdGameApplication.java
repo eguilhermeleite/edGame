46a87dc15f27c5ab35b7610215a0af6e92ac148f
Edvaldo Leite <eguilhermeleite@gmail.com>
2023-08-15 14:13:19 -0300

    created project

---
+++ src/main/java/com/leite/edvaldo/edgame/EdGameApplication.java
@@ -0,0 +1,13 @@
+package com.leite.edvaldo.edgame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdGameApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EdGameApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/leite/edvaldo/edgame/EdGameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leite.edvaldo.edgame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EdGameApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
