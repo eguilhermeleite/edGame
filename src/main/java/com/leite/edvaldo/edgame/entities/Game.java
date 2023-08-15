d41ba2833cc1fa926aadd55cd62d186bbbc7754a
Edvaldo Leite <eguilhermeleite@gmail.com>
2023-08-15 14:37:05 -0300

    Created class Game, application-test.properties & update application.properties

---
+++ src/main/java/com/leite/edvaldo/edgame/entities/Game.java
@@ -0,0 +1,30 @@
+package com.leite.edvaldo.edgame.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Entity
+public class Game {
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Include
+    @Id
+    private Long id;
+    private String title;
+
+    @Column(name = "yearGame")
+    private Integer year;
+    private String genre;
+    private String platforms;
+    private Double score;
+    private String imgUrl;
+
+    @Column(columnDefinition = "TEXT")
+    private String shortDescription;
+    @Column(columnDefinition = "TEXT")
+    private String longDescription;
+}
